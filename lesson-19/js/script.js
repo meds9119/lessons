55ba2aa1cd8e4e7396f9a283b445894670c0d57e
meds9119 <135730054+meds9119@users.noreply.github.com>
2024-01-09 16:33:04 +0100

    Add files via upload

---
+++ lesson-19/js/script.js
@@ -0,0 +1,11 @@
+let someOne = 1;
+let someTwo = 3;
+let someThree = 5;
+let someFour = 8;
+// const num = 2
+
+if (someOne === 1 && someTwo > 4 && 1 < someThree && 2 === someFour) {
+    console.log('Okay');
+} else {
+    console.log('Not okay')
+}
