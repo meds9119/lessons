095a0755c705c42ad4770802ae3cc98aee1a976e
meds9119 <135730054+meds9119@users.noreply.github.com>
2024-02-15 23:48:42 +0100

    Add files via upload

---
+++ lesson-22/js/script.js
@@ -0,0 +1,9 @@
+'use strict'
+const icon = document.querySelector('.header__icon-menu');
+
+icon.addEventListener('click', function () {
+    document.documentElement.classList.toggle('menu-open');
+
+});
+
+
