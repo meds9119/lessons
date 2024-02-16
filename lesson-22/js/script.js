25956dcf1bb6f345904d6aa71c42ec4e23f08f3e
meds9119 <135730054+meds9119@users.noreply.github.com>
2024-02-16 22:09:46 +0100

    Add files via upload

--- lesson-22/js/script.js
+++ lesson-22/js/script.js
@@ -1,5 +1,5 @@
 'use strict'
-const icon = document.querySelector('.header__icon-menu');
+const icon = document.querySelector('.menu__icon-menu');
 
 icon.addEventListener('click', function () {
     document.documentElement.classList.toggle('menu-open');
