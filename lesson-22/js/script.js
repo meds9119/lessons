955bf82670f3bc85cd49c4107d67dcc543978df5
meds9119 <135730054+meds9119@users.noreply.github.com>
2024-02-20 18:33:12 +0100

    Add files via upload

--- lesson-22/js/script.js
+++ lesson-22/js/script.js
@@ -5,5 +5,15 @@ icon.addEventListener('click', function () {
     document.documentElement.classList.toggle('menu-open');
 
 });
-
-
+// const tabletBreakpoint = parseInt(getComputedStyle(document.body).getPropertyValue('--tablet-breakpoint'));
+// console.log(tabletBreakpoint); // Перевірка значення
+// // Отримуємо потрібні елементи
+// const topheadContacts = document.querySelector('.tophead__contacts');
+// const navList = document.querySelector('.nav__list');
+//
+// // Переміщуємо елементи до відповідних місць залежно від розміру екрану
+// if (window.innerWidth <= $tablet-breakpoint) {
+//     navList.appendChild(topheadContacts); // Додати контакти у бургер-меню
+// } else {
+//     // Додати контакти до основного меню
+// }
