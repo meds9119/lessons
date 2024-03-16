fde217d1e0a2083cd763fccddb1784095afdd418
meds9119 <135730054+meds9119@users.noreply.github.com>
2024-03-16 02:33:21 +0100

    Add files via upload

--- transport-logistic/js/script.js
+++ transport-logistic/js/script.js
@@ -1,7 +1,16 @@
 'use strict'
+// Open burger-menu
 const icon = document.querySelector('.side-info__icon-menu');
 
 icon.addEventListener('click', function () {
     document.documentElement.classList.toggle('menu-open');
 
+});
+
+// Open submenu in burger-menu
+const submenuItem = document.querySelector('.nav__item--arrow');
+const submenu = document.querySelector('.submenu');
+
+submenuItem.addEventListener('click', function() {
+    submenuItem.classList.toggle('submenu-open');
 });
